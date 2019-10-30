89ba16b101b0b2746049361eef4c858c8c168983
wcss <wcss2020@gmail.com>
2019-10-30 11:27:57 +0800

    ...

--- .net & mono/Weed3Demo/demo/model/UserInfoModel.cs
+++ .net & mono/Weed3Demo/demo/model/UserInfoModel.cs
@@ -17,24 +17,41 @@ namespace Weed3Demo.demo.model
         public int city_id;
         public String name;
         public String icon;
-        
-        public void bind(GetHandlerEx s)
+
+        /// <summary>
+        /// 主要用于将查询到的数据设置到对象中
+        /// </summary>
+        /// <param name="source"></param>
+        public override void bind(GetHandlerEx source)
         {
             //1.source:数据源
             //
-            user_id = s("user_id").value<long>(0);
-            role = s("role").value<short>(0);
-            mobile = s("mobile").value("");
-            udid = s("udid").value("");
-            city_id = s("city_id").value<int>(0);
-            name = s("name").value("");
-            icon = s("icon").value("");
+            user_id = source("user_id").value<long>(0);
+            role = source("role").value<short>(0);
+            mobile = source("mobile").value("");
+            udid = source("udid").value("");
+            city_id = source("city_id").value<int>(0);
+            name = source("name").value("");
+            icon = source("icon").value("");
 
         }
 
-        public override DbTableQuery copyTo(DbTableQuery query)
+        /// <summary>
+        /// 主要用于插入或更新时的赋值操作，比如obj.copyTo(context.table("user")).insert(); 这个操作的意思是将obj中所有的值设置到table("user")中执行插入操作
+        /// </summary>
+        /// <param name="dests"></param>
+        /// <returns></returns>
+        public override DbTableQuery copyTo(DbTableQuery dests)
         {
-            
+            dests.set("user_id", user_id);
+            dests.set("role", role);
+            dests.set("mobile", mobile);
+            dests.set("udid", udid);
+            dests.set("city_id", city_id);
+            dests.set("name", name);
+            dests.set("icon", icon);
+
+            return dests;
         }
     }
 }
