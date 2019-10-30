9c8c1df163af45e32f43d158a166147728645ca2
wcss <wcss2020@gmail.com>
2019-10-30 09:36:02 +0800

    ...

---
+++ .net & mono/Weed3/IEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Noear.Weed
+{
+    /// <summary>
+    /// 数据实体抽象类
+    /// </summary>
+    public abstract class IEntity : IBinder
+    {
+        /// <summary>
+        /// 将数据复制到
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public abstract Noear.Weed.DbTableQuery set(Noear.Weed.DbTableQuery query);
+
+        /// <summary>
+        /// 数据绑定
+        /// </summary>
+        /// <param name="source"></param>
+        public abstract void bind(GetHandlerEx source);
+
+        public virtual IBinder clone()
+        {
+            return (IBinder)this.MemberwiseClone();
+        }
+    }
+}
